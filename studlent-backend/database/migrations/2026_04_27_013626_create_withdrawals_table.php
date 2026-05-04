4ae6cb69b9f2ead18f0453cb651256b50720f297
habyy-sans <muhammadhasbiyashshiddiqi@gmail.com>
2026-05-04 13:47:48 +0700

    penambahan databse

--- studlent-backend/database/migrations/2026_04_27_013626_create_withdrawals_table.php
+++ studlent-backend/database/migrations/2026_04_27_013626_create_withdrawals_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
             $table->increments('id_withdraw');
             $table->unsignedInteger('id_user')->nullable();
             $table->decimal('amount', 12, 2)->nullable();
+            $table->string('no_rekening', 50)->nullable();
+            $table->string('bank_name', 50)->nullable();
             $table->enum('status', ['pending', 'success', 'failed'])->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable(); 
